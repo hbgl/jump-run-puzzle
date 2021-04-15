15ed56e5fd870f549d7de6795610ea3415b29080
hbgl <gohbgl@gmail.com>
2021-04-15 11:56:12 +0200

    Add seesaw prototype.

---
+++ Assets/Scripts/Seesaw/ISeesawAgent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpRunPuzzle.Seesaw
+{
+    public interface ISeesawAgent
+    {
+        void SetSeesawJumpHeight(float value);
+    }
+}
--- Assets/Scripts/ThirdPersonControllerKinematic.cs
+++ Assets/Scripts/ThirdPersonControllerKinematic.cs
@@ -1,22 +1,31 @@
+using JumpRunPuzzle.Seesaw;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonControllerKinematic : MonoBehaviour
+public class ThirdPersonControllerKinematic : MonoBehaviour, ISeesawAgent
 {
     public float Speed = 6.0f;
     public float JumpHeight = 1.0f;
+    public float JumpPadJumpHeight = 5.0f;
     public float Gravity = -12f;
     public float TurnSmoothTime = 0.1f;
     public float PushPower = 2.0f;
     public float Mass = 50f;
-
+    public float SeesawLegJumpHeight = 3.0f;
+    
     private CharacterController Controller;
     private Transform Camera;
     private float VelocityY;
     private float LastFallVelocityY;
     private float TurnSmoothVelocity;
     private bool IsGrounded;
+    private bool IsOnJumpPad;
+    private bool IsOnSeesaw;
+    private float SeesawJumpHeight;
+    private Seesaw Seesaw;
+
+    private List<ControllerColliderHit> Collisions = new List<ControllerColliderHit>(12);
 
     private void Start()
     {
@@ -26,6 +35,7 @@ public class ThirdPersonControllerKinematic : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(IsGrounded);
         var wasGrounded = IsGrounded;
         var isGrounded = Controller.isGrounded;
         IsGrounded = isGrounded;
@@ -35,7 +45,7 @@ public class ThirdPersonControllerKinematic : MonoBehaviour
             if (!wasGrounded)
             {
                 LastFallVelocityY = VelocityY;
-                Debug.Log($"Last fall velocity {LastFallVelocityY}");
+                Debug.Log($"Player last fall velocity {LastFallVelocityY}");
             }
             if (velocity.y < 0)
             {
@@ -59,22 +69,109 @@ public class ThirdPersonControllerKinematic : MonoBehaviour
             velocity.z = moveVelocity.z;
         }
 
-        // Handle jumping.
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        var pushSeesawJumpHeight = 0f;
+        if (Seesaw != null)
         {
+            // Check if hitting the seesaw from above with another agent on
+            // the other side.
+            if (!wasGrounded && Seesaw.HasOtherAgent(this))
+            {
+                pushSeesawJumpHeight = Mathf.Abs(LastFallVelocityY) / 2.0f;
+
+                if (Input.GetButton("Jump"))
+                {
+                    // Boost the jump height by fixed amount, simulating pushing
+                    // the seesaw with the legs.
+                    pushSeesawJumpHeight += SeesawLegJumpHeight;
+                }
+                else
+                {
+                    // Reduce the jump height by fixed amount, simulating mass of
+                    // the seesaw and friction.
+                    pushSeesawJumpHeight -= Seesaw.JumpHeightReduction;
+                }
+
+                pushSeesawJumpHeight = Mathf.Max(pushSeesawJumpHeight, 0f);
+            }
+        }
+
+        if (pushSeesawJumpHeight >= 0.5f)
+        {
+            // Push the other agent up.
+            Seesaw.GetOtherAgent(this).SetSeesawJumpHeight(pushSeesawJumpHeight);
+        }
+        else if (SeesawJumpHeight >= 0.5f)
+        {
+            // Get pushed up by the other agent.
+            velocity.y += Mathf.Sqrt(SeesawJumpHeight * -3.0f * Gravity);
+            SeesawJumpHeight = 0f;
+        }
+        else if (Input.GetButton("Jump") && IsOnJumpPad)
+        {
+            // Get boost on jump pad.
+            velocity.y = Mathf.Sqrt(JumpPadJumpHeight * -3.0f * Gravity);
+        }
+        else if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Normal jump.
             velocity.y = Mathf.Sqrt(JumpHeight * -3.0f * Gravity);
         }
         velocity.y += Gravity * Time.deltaTime;
 
+        Collisions.Clear();
+
         Controller.Move(velocity * Time.deltaTime);
+
+        // Handle all collisions from the last move.
+        HandleCollisions();
+        Collisions.Clear();
         
         // Save current vertical velocity for gravity calculations.
         VelocityY = velocity.y;
     }
 
+    private void HandleCollisions()
+    {
+        ControllerColliderHit jumpPadHit = null;
+        ControllerColliderHit seesawHit = null;
+
+        var hitCount = Collisions.Count;
+        for (var i = 0; i < hitCount; i++)
+        {
+            var hit = Collisions[i];
+            if (jumpPadHit == null && hit.gameObject.CompareTag("JumpPad") && hit.transform.position.y <= Controller.bounds.min.y)
+            {
+                jumpPadHit = hit;
+            }
+            if (seesawHit == null && hit.gameObject.CompareTag("SeesawPad") && hit.transform.position.y <= Controller.bounds.min.y)
+            {
+                seesawHit = hit;
+            }
+        }
+
+        // Handle jump pad
+        IsOnJumpPad = jumpPadHit != null;
+
+        // Handle seesaw
+        if (seesawHit != null)
+        {
+            IsOnSeesaw = true;
+            Seesaw = seesawHit.gameObject.GetComponentInParent<Seesaw>();
+            Seesaw.SetAgent(this, seesawHit.gameObject.transform.GetSiblingIndex());
+        }
+        else if (Seesaw != null)
+        {
+            IsOnSeesaw = false;
+            Seesaw.RemoveAgent(this);
+            Seesaw = null;
+        }
+    }
+
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
+        Collisions.Add(hit);
         return;
+
         if (hit.gameObject.name == "Board")
         {
             //var force = new Vector3(0, -10f, 0);
@@ -105,5 +202,10 @@ public class ThirdPersonControllerKinematic : MonoBehaviour
 
         // Apply the push
         body.velocity = pushDir * PushPower;
+    }
+
+    public void SetSeesawJumpHeight(float value)
+    {
+        SeesawJumpHeight = value;
     }
 }
